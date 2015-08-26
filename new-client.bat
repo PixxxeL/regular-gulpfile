b9748fc181776da223e0687fd014ae40c2365b5b
pixxxel <ivan.n.sergeev@gmail.com>
2015-08-26 10:24:31 +0500

    Change batch file

--- new-client.bat
+++ new-client.bat
@@ -2,13 +2,23 @@
 
 echo ===========================================================================
 echo  1. Create bower.json, package.json
-echo  2. Download gulpfile.js
+echo  2. Download gulpfile.js and skeleton
 echo  3. Make directories
 echo.
 
 pause && npm init && bower init && ^
 npm install gulp gulp-sass gulp-jade gulp-shell gulp-coffee && ^
 curl -O https://raw.githubusercontent.com/PixxxeL/regular-gulpfile/master/gulpfile.js && ^
-mkdir jade html coffee js sass css img
+mkdir jade html coffee js sass css img && ^
+cd coffee && ^
+curl -O https://raw.githubusercontent.com/PixxxeL/regular-gulpfile/master/coffee/main.coffee && ^
+cd ..\jade && ^
+curl -O https://raw.githubusercontent.com/PixxxeL/regular-gulpfile/master/jade/base.jade && ^
+curl -O https://raw.githubusercontent.com/PixxxeL/regular-gulpfile/master/jade/index.jade && ^
+cd ..\sass && ^
+curl -O https://raw.githubusercontent.com/PixxxeL/regular-gulpfile/master/sass/_definitions.sass && ^
+curl -O https://raw.githubusercontent.com/PixxxeL/regular-gulpfile/master/sass/main.sass && ^
+cd .. && ^
+echo. && echo Job finish!
 
 exit 0
